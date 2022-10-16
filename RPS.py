5837a23427d6e8ee0075d7a9e68270fdaa2c9694
MazerJax98 <mazerjax@gmail.com>
2022-10-15 18:06:03 -0600

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -26,7 +26,7 @@ import os
         self.rectangle.move_ip(x_change, y_change)
         self.health -= 0.15'''
 
-class Rock:
+'''class Rock:
     speed = 5
     def __init__(self, rock_image, position):
         self.position = position 
@@ -41,9 +41,9 @@ class Rock:
     def move(self):
         x_change = random.randint(-Rock.speed, Rock.speed)
         y_change = random.randint(-Rock.speed, Rock.speed)
-        self.rectangle.move_ip(x_change, y_change)
+        self.rectangle.move_ip(x_change, y_change)'''
 
-class Paper:
+'''class Paper:
     speed = 5
     def __init__(self, rock_image, position):
         self.position = position 
@@ -58,9 +58,9 @@ class Paper:
     def move(self):
         x_change = random.randint(-Paper.speed, Paper.speed)
         y_change = random.randint(-Paper.speed, Paper.speed)
-        self.rectangle.move_ip(x_change, y_change)
+        self.rectangle.move_ip(x_change, y_change)'''
 
-class Scissors:
+'''class Scissors:
     speed = 5
     def __init__(self, rock_image, position):
         self.position = position 
@@ -75,6 +75,24 @@ class Scissors:
     def move(self):
         x_change = random.randint(-Scissors.speed, Scissors.speed)
         y_change = random.randint(-Scissors.speed, Scissors.speed)
+        self.rectangle.move_ip(x_change, y_change)'''
+
+
+class play_obj:
+    speed = 5
+    def __init__(self, rock_image, position):
+        self.position = position 
+        self.type = type
+        self.image = rock_image
+        self.rectangle = self.image.get_rect()
+        self.rectangle.center = position
+    
+    def draw(self, screen):
+            screen.blit(self.image, self.rectangle)
+
+    def move(self):
+        x_change = random.randint(-play_obj.speed, play_obj.speed)
+        y_change = random.randint(-play_obj.speed, play_obj.speed)
         self.rectangle.move_ip(x_change, y_change)
 
 
@@ -87,6 +105,10 @@ def main():
     print(os.path.isfile(path))
 
     print(os.getcwd()) 
+    
+    #main program
+    #
+    #
 
     pygame.init()
     size = width, height = (600, 400)
@@ -97,6 +119,12 @@ def main():
     rock_image = pygame.image.load("rock.png")
     rock_image = pygame.transform.smoothscale(rock_image, (30,30))
 
+    paper_image = pygame.image.load("paper.png")
+    paper_image = pygame.transform.smoothscale(paper_image, (30,30))
+
+    scissors_image = pygame.image.load("scissors.png")
+    scissors_image = pygame.transform.smoothscale(scissors_image, (30,30))
+
     while True:
 
         for event in pygame.event.get():
@@ -106,10 +134,17 @@ def main():
 
         screen.fill((120,120,120))
         
-        rpsObj = Rock(rock_image, (300, 200))
+        rpsObj = play_obj(rock_image, (300, 200))
         rpsObj.move()
         rpsObj.draw(screen)
 
+        rpsObj2 = play_obj(paper_image, (200,200))
+        rpsObj2.move()
+        rpsObj2.draw(screen)
+
+        rpsObj3 = play_obj(scissors_image, (400,200))
+        rpsObj3.move()
+        rpsObj3.draw(screen)
         
 
         pygame.display.update()
